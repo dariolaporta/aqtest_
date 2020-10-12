0f7911152ffd71bdf52d73a650315fa59c52f4c9
Dario <dario.laporta@temera.it>
2020-10-12 11:10:24 +0200

    inital commit

---
+++ src/assets/images/images.ts
@@ -0,0 +1,7 @@
+import Azamat from "./azamat.jpg";
+import Kevin from "./kevin.jpg";
+import Nicole from "./nicole.jpg";
+import Oladimeji from "./oladimeji.jpg";
+import Pamela from "./pamela.jpg";
+
+export { Azamat, Kevin, Nicole, Oladimeji, Pamela };
