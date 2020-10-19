8565c9c67b2d53d8aeb308af228f2ab16d39738e
Dario <dario.laporta@temera.it>
2020-10-19 11:04:07 +0200

    build

---
+++ build/precache-manifest.db20d23cc360489cf7c17de73fa25221.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0ca70ee7aac7adf6ef482593d835ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a75c0c7416e3c63cc3e2",
+    "url": "/static/css/main.2929c0f6.chunk.css"
+  },
+  {
+    "revision": "56b5c2de4adc94c051df",
+    "url": "/static/js/2.53da5b8a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.53da5b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75c0c7416e3c63cc3e2",
+    "url": "/static/js/main.4d07f427.chunk.js"
+  },
+  {
+    "revision": "3cc76c7b5f1a2ae97c68",
+    "url": "/static/js/runtime-main.272f4ebb.js"
+  },
+  {
+    "revision": "54a91b0619ccf9373d525109268219dc",
+    "url": "/static/media/Roboto-Regular.54a91b06.ttf"
+  },
+  {
+    "revision": "198a9fc6a5ac04b6b8afa8a00b3b9b49",
+    "url": "/static/media/Tungsten-Bold.198a9fc6.otf"
+  },
+  {
+    "revision": "8e2353fae1c679997470ddf270a2dab9",
+    "url": "/static/media/Tungsten-Semibold.8e2353fa.otf"
+  },
+  {
+    "revision": "b67a527adf0feaa23ef641d549e2b38f",
+    "url": "/static/media/azamat.b67a527a.jpg"
+  },
+  {
+    "revision": "230da99141e02e7a6afa3a0b1095d408",
+    "url": "/static/media/kevin.230da991.jpg"
+  },
+  {
+    "revision": "5f943fd5adaea3500a0a0aa13c36362e",
+    "url": "/static/media/nicole.5f943fd5.jpg"
+  },
+  {
+    "revision": "93a3df0e71ef765471f1627d7e7f2f88",
+    "url": "/static/media/oladimeji.93a3df0e.jpg"
+  },
+  {
+    "revision": "a1d3b385aab3a23e22c2591fbbefdf4e",
+    "url": "/static/media/pamela.a1d3b385.jpg"
+  }
+]);
